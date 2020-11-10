32f6b04b8f56da2fc02a5319890be45a642c8dda
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2020-11-10 15:39:13 -0500

    set up word, mouseover, and this functionality

--- src/js/freq.js
+++ src/js/freq.js
@@ -7,6 +7,7 @@ import '../style.scss';
 export function freq() {
 
     const IMtxt = require('url:../../data/invisible_man.txt');
+    let IM_map;
 
     // data and manipulations
     d3.text(IMtxt, d3.autoType).then((data) => {
@@ -27,13 +28,99 @@ export function freq() {
             return mp;
         });
 
-        const IM_map = xah_obj_to_map(IMobj)
+        IM_map = xah_obj_to_map(IMobj)
 
         const wordRollup = d3.rollup((cells), v => v.length, d => d)
 
-        console.log("IM text", data)
+        //console.log("IM text", data)
+        console.log("IM obj", IMobj[90]);
         console.log("IM map", IM_map);
         console.log("IM unique word map", wordRollup)
+
+        init();
     });
 
+    function init() {
+
+        const width = 800;
+        const height = 50;
+        const marginLeft = 10;
+        const marginRight = 0;
+        const marginBottom = 50;
+        const marginTop = 10;
+        const smallFont = 12;
+        const medFont = 14;
+        const bigFont = 20;
+        const lightGray = "#999";
+        const lightBlue = "#9dc1e0"; //"#bbd0e3";
+        const medGray = "#777";
+        const darkGray = "#444";
+        const word = "man"; // this will become a state variable
+
+        const keys = [...IM_map.entries()]
+            .filter(({ 1: v }) => v === word)
+            .map(([k]) => k);
+
+        const keysNum = keys.map(function (x) {
+            return parseInt(x, 10);
+        });
+
+        const svg = d3
+            .select("#d3-container-freq")
+            .append("svg")
+            .attr("viewBox", [0, 0, width, height * 1.1])
+
+        const xScale = d3
+            .scaleLinear()
+            .domain([0, 176665]) //d3.extent(keysNum)) 
+            .range([marginLeft, width - marginRight]);
+
+        const yScale = d3
+            .scaleLinear()
+            .domain([10, 0])
+            .range([0, height]);
+
+        const xAxis = d3.axisBottom(xScale);
+        svg
+            .append("g")
+            .attr("class", "axis x-axis")
+            .attr("transform", `translate(0,${height - marginBottom})`)
+            // .call(xAxis)
+            .append("text")
+            .attr("class", "axis-label")
+            .attr("x", "45%")
+            .attr("dy", "3em")
+            .text(`dispersion of "${word}" in Invisible Man`)
+            .attr("font-size", medFont)
+            .attr("fill", "none");
+
+        const yAxis = d3.axisLeft(yScale);
+        svg
+            .append("g")
+            .attr("class", "axis y-axis")
+            .attr("transform", `translate(${marginLeft},${marginTop})`)
+            // .call(yAxis)
+            .append("text")
+            .attr("class", "axis-label")
+            .attr("y", "50%")
+            .attr("dx", "-3em")
+            .attr("writing-mode", "vertical-rl")
+            .text(" ")
+            .attr("font-size", medFont)
+            .attr("fill", medGray)
+
+        // create circle for each key (word instance)
+        const circle =
+            svg.selectAll("circle")
+                .data(keysNum)
+                .join("circle")
+                .attr("cy", yScale(5))
+                .attr("cx", (d) => xScale(d))
+                .attr("r", 1)
+                .attr("fill", lightBlue)
+                .attr("opacity", 1)
+                .attr("stroke", lightBlue)
+
+
+    }
 }
--- src/js/text.js
+++ src/js/text.js
@@ -5,32 +5,138 @@ import '../style.scss';
 
 export function text() {
 
-    const IMtxt = require('url:../../data/invisible_man.txt');
+    // CONSTANTS AND GLOBALS
 
-    // data and manipulations
+    const widthW = window.innerWidth * 0.8;
+    const heightW = window.innerHeight * 0.9;
+    const marginLeft = 0;
+    const marginRight = 0;
+    const marginBottom = 50;
+    const marginTop = 10;
+    const lightBlue = "#9dc1e0"; //"#bbd0e3";
+    const IMtxt = require('url:../../data/invisible_man.txt');
+    let IMobj;
+    let IM_map;
+
+    // DATA AND MANIPULATIONS
     d3.text(IMtxt, d3.autoType).then((data) => {
         data = data.slice(515, -198);
         const spaceRE = /\s+/g;
         const punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
-        const IM_noMeta_noPunct = data.replace(punctRE, '').replace(spaceRE, ' ')
-        const cells = IM_noMeta_noPunct.toLowerCase().split(/\s+/)
+        const IM_noMeta_noPunct = data.replace(punctRE, '').replace(spaceRE, ' ');
+        const cells = IM_noMeta_noPunct.toLowerCase().split(/\s+/);
 
-        // const IMobj = cells.reduce(function (acc, cur, i) {
-        //     acc[i] = cur;
-        //     return acc;
-        // }, {});
+        IMobj = cells.reduce(function (acc, cur, i) {
+            acc[i] = cur;
+            return acc;
+        }, {});
 
-        // const xah_obj_to_map = (obj => {
-        //     const mp = new Map;
-        //     Object.keys(obj).forEach(k => { mp.set(k, obj[k]) });
-        //     return mp;
-        // });
+        const xah_obj_to_map = (obj => {
+            const mp = new Map;
+            Object.keys(obj).forEach(k => { mp.set(k, obj[k]) });
+            return mp;
+        });
 
-        // const IM_map = xah_obj_to_map(IMobj)
+        IM_map = xah_obj_to_map(IMobj)
 
-        // const wordRollup = d3.rollup((cells), v => v.length, d => d)
+        const wordRollup = d3.rollup((cells), v => v.length, d => d)
+        //console.log("IM map get", d => IM_map.get(d))
+        init();
+
     });
 
-    //print data in <div id="d3-container"></div>
+    // TEXT ACROSS SCREEN
+    function init() {
 
+        const svg = d3
+            .select("#d3-container")
+            .append("svg")
+            .attr('width', widthW)
+            .attr('height', heightW * .6);
+
+        function gridData() {
+            var data = new Array();
+            var num = 0;
+            var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+            var ypos = 1;
+            var width = widthW / 20;
+            var height = 15;
+            var click = 0;
+            var wordlength = 1;
+
+            // iterate for rows	
+            for (var row = 0; row < 100; row++) {
+                data.push(new Array());
+
+                // iterate for cells/columns inside rows
+                for (var column = 0; column < 20; column++) {
+                    data[row].push({
+                        num: num,
+                        word: IMobj[num],
+                        wordlength: (IMobj[num]).length,
+                        x: xpos,
+                        y: ypos,
+                        width: width,
+                        height: height,
+                        click: click
+                    })
+                    // increment the x position by the word length + a set spacing amount
+                    xpos += 6 * (IMobj[num]).length + 8;
+                    // increment the count of each square by 1
+                    num += 1;
+                }
+                // reset the x position after a row is complete
+                xpos = 1;
+                // increment the y position for the next row. Move it down 50 (height variable)
+                ypos += height;
+            }
+            return data;
+        }
+
+        var gridData = gridData();
+        // I like to log the data to the console for quick debugging
+        console.log(gridData);
+        //console.log("num", d3.extent(gridData));
+
+        var grid = d3.select("#grid")
+            .append("svg")
+            .attr("width", widthW)
+            .attr("height", "510px");
+
+        var row = grid.selectAll(".row")
+            .data(gridData)
+            .enter().append("g")
+            .attr("class", "row");
+
+        var column = row.selectAll(".square")
+            .data(function (d) { return d; })
+            .enter().append("rect")
+            .attr("class", "square")
+            .attr("x", function (d) { return d.x; })
+            .attr("y", function (d) { return d.y; })
+            .attr("width", function (d) { return d.width; })
+            .attr("height", function (d) { return d.height; })
+            .style("fill", "#3a2224")
+            .style("stroke", "#fff0")
+
+        var text = row.selectAll(".label")
+            .data(function (d) { return d; })
+            .join("svg:text")
+            .attr("x", function (d) { return d.x + d.width / 2 })
+            .attr("y", function (d) { return d.y + d.height / 2 })
+            .attr("text-anchor", "left")
+            .attr("dy", ".5em")
+            .attr("font-size", 10)
+            .style("fill", "fff")
+            .text(function (d) { return d.word })
+            .on('mouseenter', function (d) {
+                d3.select(this)
+                    .style("fill", lightBlue)
+            })
+            .on('mouseout', function (d) {
+                d3.select(this)
+                    .style("fill", "fff")
+            });
+
+    }
 }
